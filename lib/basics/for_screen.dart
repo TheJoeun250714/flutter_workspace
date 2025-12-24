442d6a87565954a0adc73df27edcc3c5669c81ba
tje0714 <tje0714@outlook.kr>
2025-12-24 13:20:34 +0900

    void function if for

--- lib/basics/for_screen.dart
+++ lib/basics/for_screen.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
 
-class ForScreen extends StatelessWidget {
+class ForScreen extends StatefulWidget {
   const ForScreen({super.key});
 
-  // appbar 이용해서 메인으로 가는 버튼 생성
+  @override
+  State<ForScreen> createState() => _ForScreen();
+}
 
+class _ForScreen extends State<ForScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('For 반복문 확인하기'),
+        backgroundColor: Colors.yellow,
+        // 돌아가기 아이콘 추가
+      ),
       body: Center(
-        child: Text('for_screen is working'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('반복문 화면')
+          ],
+        ),
+
       ),
     );
   }
